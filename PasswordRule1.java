f6cb5a8ebc1ace78eabaca05ccef874b00573f12
Deepak20233 <127834609+Deepak20233@users.noreply.github.com>
2023-05-09 19:05:42 +0530

    Added: Password Rule 1

---
+++ PasswordRule1.java
@@ -0,0 +1,31 @@
+package com.java;
+
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+import java.util.Scanner;
+public class PasswordRule1 {
+
+	public static void main(String[] args) {
+		validatePassword();
+
+	}
+	@Test
+	public static void validatePassword() {
+		Scanner passwordInput = new Scanner(System.in);
+		System.out.println("Enter password: ");
+		String password = passwordInput.nextLine();
+/*
+ * Password rule - 1: Minimum 8 characters
+ */
+	    String regex = "^[a-zA-z0-9!@#$%^&*()?]{8,}$"; // UC5
+		Pattern pattern = Pattern.compile(regex);
+		if (pattern.matcher(password).matches()) {
+			System.out.println("Password: " + password + " is valid.");
+		} else {
+			System.out.println("Password: " + password + " is invalid.");
+		}
+
+	}
+}
