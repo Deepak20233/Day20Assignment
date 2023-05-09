e2f84fa868bf4d1d5151718c9eae5d3766e49bca
Deepak20233 <127834609+Deepak20233@users.noreply.github.com>
2023-05-09 19:01:07 +0530

    Added: Last Name

---
+++ LastName.java
@@ -0,0 +1,28 @@
+package com.java;
+import java.util.*;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+public class LastName {
+    
+	public static void main(String[] args) {
+		validateLastName();
+
+	}
+	@Test
+	public static void validateLastName() {
+		Scanner lNameInput = new Scanner(System.in);
+		System.out.println("Enter last name: ");
+		String lName = lNameInput.nextLine();
+
+		String regex = "^[A-Z][a-zA-Z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+
+		if (pattern.matcher(lName).matches()) {
+			System.out.println("Last name: " + lName + " is valid.");
+		} else {
+			System.out.println("Last name: " + lName + " is invalid.");
+		}
+
+	}
+}
